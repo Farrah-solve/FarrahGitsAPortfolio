612c7138052fa7e6711c24fed7d60ede83c2ac25
Farrah-solve <68975737+Farrah-solve@users.noreply.github.com>
2021-01-29 00:02:16 -0500

    for now

--- farrahgitsaportfolio/src/App.js
+++ farrahgitsaportfolio/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Introduction from './components/introduction'
+import Animate from './components/animate'
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
          	
 				<div id="colorlib-main">
 					<Introduction></Introduction>
-					
+					<Animate></Animate>
           	</div>
       	</div>
       </div>
